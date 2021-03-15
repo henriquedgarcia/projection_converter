c308a611c83c32d00096faf3bf1f751efe2e30f5
Henrique D. Garcia <henriquedgarcia@gmail.com>
2021-03-15 20:39:04 -0300

    Correct config path.

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def converter(in_file, out_file, conversion, overwrite=False):
     template = read_template()
     params['InputFile'] = f'{in_file}'
     config = template.format(**params)
-    config_file = f'temp/{out_file[:-4]}.cfg'
+    config_file = f'{out_file[:-4]}.cfg'
     save_config(config, config_file)
 
     command = f'bin/TApp360ConvertStatic -c {config_file} -OutputFile {out_file}'
