4cc4d56dd879cc28b04aaf0a411a1f92163d5d10
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2021-06-24 23:05:21 -0300

    Big upgrade

--- main.py
+++ main.py
@@ -1,155 +1,164 @@
+#!/usr/bin/env
 import glob
 import os
-import subprocess
 import sys
 import time
-
-erp_params = {'SourceWidth'       : '3840',
-              'SourceHeight'      : '2160',
-              'InputFile'         : None,
-              'InputGeometryType' : '0',
-              'SourceFPStructure' : '1 1   0 0',
-              'CodingGeometryType': '1',
-              'CodingFPStructure' : '2 3   4 0 0 0 5 0   3 180 1 270 2 0',
-              'CodingFaceWidth'   : 960,
-              'CodingFaceHeight'  : 960,
-              }
-cmp_params = {'SourceWidth'       : '2880',
-              'SourceHeight'      : '1920',
-              'InputFile'         : None,
-              'InputGeometryType' : '1',
-              'SourceFPStructure' : '2 3   4 0 0 0 5 0   3 180 1 270 2 0',
-              'CodingGeometryType': '0',
-              'CodingFPStructure' : '1 1   0 0',
-              'CodingFaceWidth'   : 3840,
-              'CodingFaceHeight'  : 2160,
-              }
+import json
+from pathlib import Path
 
 
-def main(folder, conversion, duration=None, overwrite=False, remove_yuv=False):
-    for in_file in glob.glob(f'{folder}/*.mp4'):
-        name = os.path.split(in_file)[1][:-4]
-        compressed_file = f'output/{name}_{conversion}.mp4'
-        uncompressed_file = f'temp/{name}.yuv'
-        converted_file = f'temp/{name}_{conversion}.yuv'
+class Converter:
+    in_file: str = None
+    config: dict = None
+    app = 'bin/360ConvertAppStatic'
+    uncompressed = False
 
-        if os.path.exists(compressed_file) and not overwrite:
-            print(f'{compressed_file} exist. Skipping.')
-            continue
+    @property
+    def compressed_file(self):
+        return f'output/{self.name}_{self.conversion}.mp4'
+
+    @property
+    def name(self):
+        return os.path.split(self.in_file)[1][:-4]
+
+    @property
+    def uncompressed_file(self):
+        if self.uncompressed:
+            path = f'{self.folder}/{self.name}.yuv'
+        else:
+            path = f'temp/{self.name}.yuv'
+
+        return path
+
+    @property
+    def converted_file(self):
+        return f'temp/{self.name}_converted_{self.conversion}.yuv'
+
+    def __init__(self, origin_folder, conversion, duration=None, overwrite=False, remove_yuv=False):
+        self.folder = Path(origin_folder)
+        self.conversion = conversion
+        self.duration = duration
+        self.overwrite = overwrite
+        self.remove_yuv = remove_yuv
 
         os.makedirs('temp/', exist_ok=True)
 
-        uncompress(in_file, uncompressed_file, conversion, duration, overwrite)
-        converter(uncompressed_file, converted_file, conversion, overwrite)
-        compress(converted_file, compressed_file, conversion, overwrite)
+        self._config()
+        self._run()
 
-        if remove_yuv:
-            os.remove(uncompressed_file)
-            os.remove(converted_file)
-    # shutil.rmtree('temp')
+    def _config(self):
+        with open('config.json', 'r', encoding='utf-8') as f:
+            self.config = json.load(f)
+        try:
+            self.params = self.config[self.conversion]
+        except KeyError:
+            KeyError('Conversion mode not exist.')
 
+    def _run(self):
+        for self.in_file in glob.glob(f'{self.folder}/*.mp4'):
+            self.uncompress()
+            self.converter()
+            self.compress()
 
-def uncompress(in_file, out_file, conversion, duration=None, overwrite=False):
-    if os.path.exists(out_file) and not overwrite:
-        print(f'{out_file} exist. Skipping.')
-        return
+            if self.remove_yuv:
+                os.remove(self.uncompressed_file)
+                os.remove(self.converted_file)
 
-    if conversion == 'cmp2erp':
-        input_scale = '2880x1920'
-        dar = '3/2'
-    elif conversion == 'erp2cmp':
-        input_scale = '3840x2160'
-        dar = '2'
-    else:
-        raise KeyError(f'Conversão {conversion} não suportada.')
+        for self.in_file in glob.glob(f'{self.folder}/*.yuv'):
+            self.uncompressed = True
+            self.converter()
+            self.compress()
 
-    command = f'ffmpeg -y -i {in_file} -vf "scale={input_scale},setdar={dar}" '
-    if duration:
-        command += f'-t {duration} '
-    command += f'{out_file}'
-    run_command(command, f'{out_file[:-4]}.log')
+            os.remove(self.converted_file)
 
+    def uncompress(self):
+        input_scale = self.params['input_scale']
+        dar = self.params['dar']
+        in_file = self.in_file
+        out_file = self.uncompressed_file
+        duration = self.duration
+        log_file = f'{out_file[:-4]}.log'
+        overwrite = self.overwrite
 
-def converter(in_file, out_file, conversion, overwrite=False):
-    if os.path.exists(out_file) and not overwrite:
-        print(f'{out_file} exist. Skipping.')
-        return
+        if os.path.exists(out_file) and not overwrite:
+            print(f'{self.uncompressed_file} exist. Skipping.')
+            return
 
-    if conversion == 'erp2cmp':
-        params = erp_params
-        output_scale = '2880x1920'
-        cf = 1
-    elif conversion == 'cmp2erp':
-        params = cmp_params
-        output_scale = '3840x2160'
-        cf = 0
-    else:
-        raise KeyError(f'Conversão {conversion} não suportada.')
+        command = f'ffmpeg -y -i {in_file} -vf "scale={input_scale},setdar={dar}" '
+        if duration:
+            command += f'-t {duration} '
 
-    template = read_template()
-    params['InputFile'] = f'{in_file}'
-    params['OutputFile'] = f'{out_file}'
-    config = template.format(**params)
-    config_file = f'{out_file[:-4]}.cfg'
-    save_config(config, config_file)
+        command += f'{out_file} &| tee {log_file}'
+        self.run_command(command)
 
-    command = f'bin/TApp360ConvertStatic -c {config_file}'
+    def converter(self):
+        in_file = self.uncompressed_file
+        out_file = self.converted_file
+        log_file = f'{out_file[:-4]}.log'
+        overwrite = self.overwrite
 
-    if sys.platform.startswith('win32'):
-        command = f'bash -c "{command}"'
-    run_command(command, f'{out_file[:-4]}.log')
+        if os.path.exists(out_file) and not overwrite:
+            print(f'{out_file} exist. Skipping.')
+            return
 
-    boring_name = out_file.replace('.yuv', f'_{output_scale}x8_cf1.yuv')
-    if os.path.exists(boring_name):
-        os.renames(boring_name, out_file)
+        params = self.params['params']
+        params['InputFile'] = f'{in_file}'
+        params['OutputFile'] = f'{out_file}'
 
+        template = self.read_template()
+        config = template.format(**params)
 
-def compress(in_file, out_file, conversion, overwrite=False):
-    if os.path.exists(out_file) and not overwrite:
-        print(f'{out_file} exist. Skipping.')
-        return
+        config_file = f'{out_file[:-4]}.cfg'
+        self.save_config(config, config_file)
+        command = f'{self.app} -c {config_file} &| tee {log_file}'
 
-    if conversion == 'erp2cmp':
-        output_scale = '2880x1920'
-    elif conversion == 'cmp2erp':
-        output_scale = '3840x2160'
-    else:
-        raise KeyError(f'Conversão {conversion} não suportada.')
+        self.run_command(command)
 
-    command = (f'ffmpeg -y -f rawvideo -video_size {output_scale} -framerate 30'
-               f' -i {in_file} -crf 17 {out_file}')
-    run_command(command, f'{out_file[:-4]}.log')
+        output_scale = self.params['output_scale']
+        boring_name = out_file.replace('.yuv', f'_{output_scale}x8_cf1.yuv')
+        if os.path.exists(boring_name):
+            os.renames(boring_name, out_file)
 
+    def compress(self):
+        in_file = self.converted_file
+        out_file = self.compressed_file
+        log_file = f'{out_file[:-4]}.log'
+        overwrite = self.overwrite
+        output_scale = self.params['output_scale']
 
-def save_config(config, filename):
-    with open(filename, 'w', encoding='utf-8') as fd:
-        fd.write(config)
+        if os.path.exists(out_file) and not overwrite:
+            print(f'{out_file} exist. Skipping.')
+            return
 
+        command = (f'ffmpeg -y -f rawvideo -video_size {output_scale} -framerate 30'
+                   f' -i {in_file} -crf 0 {out_file} &| tee {log_file}')
 
-def read_template() -> str:
-    with open('template.cfg', 'r') as fd:
-        template = fd.read()
-    return template
+        self.run_command(command)
 
+    @staticmethod
+    def save_config(config, filename):
+        with open(filename, 'w', encoding='utf-8') as fd:
+            fd.write(config)
 
-def run_command(command: str, logfile):
-    print(command)
-    start_time = time.time()
-    if logfile:
-        with open(logfile, 'w', encoding='utf-8') as f:
-            subprocess.run(command, shell=True, encoding='utf-8', stdout=f,
-                           stderr=subprocess.STDOUT)
-    print(f'\nCompleted in {int(time.time() - start_time)} seg')
+    @staticmethod
+    def read_template() -> str:
+        with open('template.cfg', 'r') as fd:
+            template = fd.read()
+        return template
 
-    # process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-    #                            stderr=subprocess.STDOUT, encoding='utf-8')
-    # while True:
-    #     if process.poll() is not None:
-    #         break
-    #     print('.', end='', flush=True)
-    #     time.sleep(1)
+    @staticmethod
+    def run_command(command: str):
+        print(command)
+        start_time = time.time()
+
+        # Method 3
+        if sys.platform.startswith('win32'):
+            command = f'bash -c "{command}"'
+        os.system(command)
+
+        print(f'\nCompleted in {int(time.time() - start_time)} seg.')
 
 
 if __name__ == '__main__':
-    main('erp', 'erp2cmp', duration=None, overwrite=False, remove_yuv=False)
-    main('cmp', 'cmp2erp', duration=None, overwrite=False, remove_yuv=False)
+    # Converter(origin_folder='erp', conversion='erp2cmp', duration=1, overwrite=False, remove_yuv=False)
+    Converter(origin_folder='cmp', conversion='cmp2erp', duration=None, overwrite=False, remove_yuv=False)
